6668c59d789ac67c8fb8da879778b688abc28632
Aidan Chang <119556552+plinygg@users.noreply.github.com>
2025-04-17 23:33:27 -0500

    css is working now

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / "myapp/static",
+    # os.path.join(BASE_DIR, 'static')
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
